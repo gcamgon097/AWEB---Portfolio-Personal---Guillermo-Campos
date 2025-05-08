7ab5352370ae2f60e5856b97a69cb1e73f0d45ec
mlopron829 <mlopron829@g.educaand.es>
2025-05-08 12:41:43 +0200

    cambio 2 en el seguimiento 3

--- js/script.js
+++ js/script.js
@@ -7,37 +7,40 @@ const items = document.querySelectorAll('.porfolio__itm');
 const prevBtn = document.querySelector('.porfolio__btn--prev'); 
 const nextBtn = document.querySelector('.porfolio__btn--next');
 
-console.log(items);
-/*Estado del carrusel */
-let indiceActual = 0; // Índice del proyecto que se está mostrando
-const totalProyectos = items.length; // Número total de proyectos en el carrusel
+if(carru & items & prevBtn & nextBtn){
+  /*Estado del carrusel */
+  let indiceActual = 0; // Índice del proyecto que se está mostrando
+  const totalProyectos = items.length; // Número total de proyectos en el carrusel
 
-/*Función que actualiza la posición del carrusel*/
-function actualizarCarrusel() {
-  const desplazamiento = -indiceActual * 100; // Calcula el desplazamiento en %
-  carru.style.transform = `translateX(${desplazamiento}%)`; // Aplica transformación al contenedor
+  /*Función que actualiza la posición del carrusel*/
+  function actualizarCarrusel() {
+    const desplazamiento = -indiceActual * 100; // Calcula el desplazamiento en %
+    carru.style.transform = `translateX(${desplazamiento}%)`; // Aplica transformación al contenedor
+  }
+
+  /*Evento para botón "Anterior"*/
+  prevBtn.addEventListener('click', () => {
+    indiceActual = (indiceActual - 1 + totalProyectos) % totalProyectos; // Mueve al anterior (cíclico)
+    console.log(indiceActual);
+    console.log(items[indiceActual]);
+    actualizarCarrusel();
+    //Investigar claslist y funcion toogle*/
+  });
+
+  /*Evento para botón "Siguiente" */
+  nextBtn.addEventListener('click', () => {
+    indiceActual = (indiceActual + 1) % totalProyectos; // Mueve al siguiente (cíclico)
+    console.log(indiceActual);
+    console.log(items[indiceActual]);
+    actualizarCarrusel();
+  });
+
+
+  /*Inicializa el carrusel mostrando el primer proyecto*/
+  actualizarCarrusel();
+
 }
 
-/*Evento para botón "Anterior"*/
-prevBtn.addEventListener('click', () => {
-  indiceActual = (indiceActual - 1 + totalProyectos) % totalProyectos; // Mueve al anterior (cíclico)
-  console.log(indiceActual);
-  console.log(items[indiceActual]);
-  actualizarCarrusel();
-  //Investigar claslist y funcion toogle*/
-});
-
-/*Evento para botón "Siguiente" */
-nextBtn.addEventListener('click', () => {
-  indiceActual = (indiceActual + 1) % totalProyectos; // Mueve al siguiente (cíclico)
-  console.log(indiceActual);
-  console.log(items[indiceActual]);
-  actualizarCarrusel();
-});
-
-
-/*Inicializa el carrusel mostrando el primer proyecto*/
-actualizarCarrusel();
 
 /*Boton Claro Oscuro*/
 let darkmode = localStorage.getItem('darkmode')
